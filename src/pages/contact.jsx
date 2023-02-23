ec5e975f49e6860748d6d84554bc90836d08074e
LeoGarrison <you@example.com>
2023-02-23 13:15:49 -0800

    initial commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,15 @@
+import city from "../assets/City.png";
+import clock from "../assets/Clock.png";
+
+const contact = () => {
+    return (
+        <>
+            <section id="contact">
+                <img id="city" src={city} /> 
+                <img id="clock" src={clock} />
+            </section>
+        </>
+    )
+}
+
+export default contact;
